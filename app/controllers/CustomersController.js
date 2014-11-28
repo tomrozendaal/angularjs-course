7198a24ebbc929d760764d7b81f5ff9d877ac3a6
Tom Rozendaal <t.rozendaal@finalist.nl>
2014-11-28 11:35:46 +0100

    Using filters

---
+++ app/controllers/CustomersController.js
@@ -0,0 +1,16 @@
+function CustomersController($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse = 'false';
+
+    $scope.customers= [
+        {joined: '2000-12-02', name: 'John', city: 'New York', orderTotal: 9.9956},
+        {joined: '2004-08-02', name: 'Zed', city: 'Rotterdam', orderTotal: 5.356},
+        {joined: '2014-11-05', name: 'Aap', city: 'Amsterdam', orderTotal: 44.9123},
+        {joined: '2014-10-11', name: 'Bas', city: 'Papendrecht', orderTotal: 12.9123}
+    ];
+
+    $scope.doSort = function(propName) {
+        $scope.sortBy=propName;
+        $scope.reverse=!$scope.reverse;
+    };
+}
