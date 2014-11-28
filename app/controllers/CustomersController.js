1f31f0913b355e40940ef053da639acc3c5b57af
Tom Rozendaal <t.rozendaal@finalist.nl>
2014-11-28 12:00:47 +0100

    Modules

--- app/controllers/CustomersController.js
+++ app/controllers/CustomersController.js
@@ -1,4 +1,5 @@
-function CustomersController($scope) {
+// Option 1
+/*app.controller('CustomersController', function ($scope) {
     $scope.sortBy = 'name';
     $scope.reverse = 'false';
 
@@ -13,4 +14,48 @@ function CustomersController($scope) {
         $scope.sortBy=propName;
         $scope.reverse=!$scope.reverse;
     };
-}
+});*/
+
+// Option 2
+/*(function() {
+
+    angular.module('customersApp').controller('CustomersController', function ($scope) {
+        $scope.sortBy = 'name';
+        $scope.reverse = 'false';
+
+        $scope.customers= [
+            {joined: '2000-12-02', name: 'John', city: 'New York', orderTotal: 9.9956},
+            {joined: '2004-08-02', name: 'Zed', city: 'Rotterdam', orderTotal: 5.356},
+            {joined: '2014-11-05', name: 'Aap', city: 'Amsterdam', orderTotal: 44.9123},
+            {joined: '2014-10-11', name: 'Bas', city: 'Papendrecht', orderTotal: 12.9123}
+        ];
+
+        $scope.doSort = function(propName) {
+            $scope.sortBy=propName;
+            $scope.reverse=!$scope.reverse;
+        };
+    });
+}());*/
+
+// Option 3
+(function() {
+
+    var CustomersController = function ($scope) {
+        $scope.sortBy = 'name';
+        $scope.reverse = 'false';
+
+        $scope.customers= [
+            {joined: '2000-12-02', name: 'John', city: 'New York', orderTotal: 9.9956},
+            {joined: '2004-08-02', name: 'Zed', city: 'Rotterdam', orderTotal: 5.356},
+            {joined: '2014-11-05', name: 'Aap', city: 'Amsterdam', orderTotal: 44.9123},
+            {joined: '2014-10-11', name: 'Bas', city: 'Papendrecht', orderTotal: 12.9123}
+        ];
+
+        $scope.doSort = function(propName) {
+            $scope.sortBy=propName;
+            $scope.reverse=!$scope.reverse;
+        };
+    }
+
+    angular.module('customersApp').controller('CustomersController', CustomersController);
+}());
