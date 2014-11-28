2365eb7aa3b005243bb8ed63b51520a1ed57eb80
Tom Rozendaal <t.rozendaal@finalist.nl>
2014-11-28 16:03:15 +0100

    Animations

--- app/services/customersFactory.js
+++ app/services/customersFactory.js
@@ -10,6 +10,14 @@
             return $http.get('/customers/' + customerId);
         }
 
+        factory.getOrders = function() {
+            return $http.get('/orders');
+        }
+
+        factory.deleteCustomer = function(customerId) {
+            return $http.delete('/customers/' + customerId);
+        }
+
         return factory;
     };
 
